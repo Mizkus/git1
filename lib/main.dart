f45ee3b255735e38dc39eef57ddcc97bd336eb2f
mizkus <kirill131c@gmail.com>
2019-10-21 15:16:26 +0800

    upd

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,13 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LayoutBuilder(
-        builder: (context, constraints) {
-          if(constraints.maxWidth > 1000){return Desktop();}
-          else{return Mobile();}
-        }
-      )
-    );
+    return MaterialApp(home: LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth > 1000) {
+        return Desktop();
+      } else {
+        return Mobile();
+      }
+    }));
   }
 }
 
@@ -22,57 +21,62 @@ class Desktop extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: Row(
-                children: <Widget>[
-                  Expanded(child: Container(),),
-                  Expanded(
+      children: <Widget>[
+        Expanded(
+            flex: 1,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(),
+                ),
+                Expanded(
                     child: Container(
-                      child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Text('About me'),
-                          Text('Work'),
-                          Text('Contact')
-                        ],
-                     )
-                    )
-                  )
-                ],
-              )
-            ),
-            Expanded(
-              flex: 6,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
+                        child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text('About me'),
+                    Text('Work'),
+                    Text('Contact')
+                  ],
+                )))
+              ],
+            )),
+        Expanded(
+            flex: 6,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
                     child: Container(
-                      child: Column(children: <Widget>[
-                        CircleAvatar(
-                          maxRadius: 250,
-                          backgroundImage: NetworkImage('http://www.simpsons-art.ru/personas/bart.png'),
-                          ),            
-                        Text(
-                          'Kirill Borodin',
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
-                        ),
-                        Text('student')
-                      ],)
-                    )
-                  ),
-                  Expanded(child: Container(child: Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[Text('Student of grammar school number one')],)))
-                ],
-              )
-            )
-          ],
-        )
-    );
+                        child: Column(
+                  children: <Widget>[
+                    CircleAvatar(
+                      maxRadius: 250,
+                      backgroundImage: NetworkImage(
+                          'http://www.simpsons-art.ru/personas/bart.png'),
+                    ),
+                    Text(
+                      'Kirill Borodin',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+                    ),
+                    Text('student')
+                  ],
+                ))),
+                Expanded(
+                    child: Container(
+                        child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Student of grammar school number one')
+                  ],
+                )))
+              ],
+            ))
+      ],
+    ));
   }
 }
-
 
 class Mobile extends StatelessWidget {
   @override
@@ -81,40 +85,53 @@ class Mobile extends StatelessWidget {
       body: Column(
         children: <Widget>[
           Expanded(
-            flex: 8,
-            child: Container(
-              child: Column(children: <Widget>[
-                CircleAvatar(
-                  maxRadius: 200,
-                  backgroundImage: NetworkImage('http://www.simpsons-art.ru/personas/bart.png'),
-              ), 
-            ],)
-          )
-        ),
-        Expanded(flex: 5,child: Container(child: Column(children: <Widget>[Text(
-                    'Kirill Borodin',
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+              flex: 8,
+              child: Container(
+                  child: Column(
+                children: <Widget>[
+                  CircleAvatar(
+                    maxRadius: 200,
+                    backgroundImage: NetworkImage(
+                        'http://www.simpsons-art.ru/personas/bart.png'),
                   ),
-                  Text('student')],),),),
-        Expanded(
-          flex: 2,
-          child: Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text('About me'),
-                Text('Work'),
-                Text('Contact')
-              ],
-            )
-          )
-        ),
-        Expanded(flex: 2,child: Container(child: Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[Text('Student of grammar school number one')],)))
-      ],),
+                ],
+              ))),
+          Expanded(
+            flex: 5,
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    'Kirill Borodin',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+                  ),
+                  Text('student')
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+              flex: 2,
+              child: Container(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text('About me'),
+                  Text('Work'),
+                  Text('Contact')
+                ],
+              ))),
+          Expanded(
+              flex: 2,
+              child: Container(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Student of grammar school number one')
+                ],
+              )))
+        ],
+      ),
     );
   }
 }
-
-
-
-  
